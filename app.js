17054024e90307e499df0bf66c17173b6164cdf3
sarin punk <sarin.bhattarai@icp.edu.np>
2023-05-03 14:43:55 +0545

    feat(chhccc)

--- app.js
+++ app.js
@@ -30,10 +30,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-store");
-  next();
-});
 app.use("/uploads", (_, res, next) => {
   res.set("Cross-Origin-Resource-Policy", "cross-origin");
   next();
